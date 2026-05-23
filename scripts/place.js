126a9ef1e4e360ff1b57a78326255592e69593e1
chinonso daniel <chinonsodanielechefu@gmail.com>
2026-05-23 15:12:06 +0100

    redo 1

--- scripts/place.js
+++ scripts/place.js
@@ -1,24 +1,19 @@
-const temp = 30; // °C
-const wind = 10; // km/h
+const temp = 30;
+const wind = 10;
 
 function calculateWindChill(t, v) {
-  return (
-    13.12 +
-    0.6215 * t -
-    11.37 * Math.pow(v, 0.16) +
-    0.3965 * t * Math.pow(v, 0.16)
-  );
+  return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
 }
 
 let chill = "N/A";
 
-// Wind chill conditions (Metric)
+// ONLY run if conditions are met
 if (temp <= 10 && wind > 4.8) {
   chill = calculateWindChill(temp, wind).toFixed(1);
 }
 
 document.getElementById("chill").textContent = chill;
 
-// FOOTER INFO
+// FOOTER
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
